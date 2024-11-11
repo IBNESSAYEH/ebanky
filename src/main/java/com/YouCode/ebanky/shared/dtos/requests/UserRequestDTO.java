ab322eed40360a01412811b97de65382f19d3959
abdellatif ibnessayeh <abdellatifibnessayeh@gmail.com>
2024-11-11 15:30:14 +0100

    add userRequestDto vzlidation

--- src/main/java/com/YouCode/ebanky/shared/dtos/requests/UserRequestDTO.java
+++ src/main/java/com/YouCode/ebanky/shared/dtos/requests/UserRequestDTO.java
@@ -1,13 +1,31 @@
 package com.YouCode.ebanky.shared.dtos.requests;
 
 
-
+import jakarta.validation.constraints.*;
 
 public class UserRequestDTO {
+
+    @NotNull
+    @NotBlank(message = "First name cannot be blank")
+    @Size(min = 3, max = 50, message = "First name must be between 3 and 50 characters")
     private String firstName;
+
+    @NotNull
+    @NotBlank(message = "Last name cannot be blank")
+    @Size(min = 3, max = 50, message = "Last name must be between 3 and 50 characters")
     private String lastName;
+
+    @NotNull
+    @NotBlank(message = "Email cannot be blank")
+    @Email(message = "Email should be valid")
     private String email;
+    @NotNull
+    @NotBlank(message = "Password cannot be blank")
+    @Size(min = 8, max = 50, message = "Password must be between 3 and 50 characters")
     private String password;
+
+    @NotNull
+    @Min(value = 18, message = "Age must be at least 18")
     private int age;
     private Double monthlyIncome;
     private int creditScore;
